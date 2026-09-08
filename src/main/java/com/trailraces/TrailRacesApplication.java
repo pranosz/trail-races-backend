de3c1139b56b6a056958c01f82079c30ab53c75f
Piotr Ranosz <pranosz@wp.pl>
2026-09-08 21:51:14 +0200

    Initialize Spring Boot backend

---
+++ src/main/java/com/trailraces/TrailRacesApplication.java
@@ -0,0 +1,13 @@
+package com.trailraces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailRacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrailRacesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trailraces/TrailRacesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trailraces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrailRacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
